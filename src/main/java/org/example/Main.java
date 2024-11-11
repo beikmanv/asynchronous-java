33066e89fc82dceb0e610f49708172fa48398bed
Valdis <v.beikmanis@gmail.com>
2024-11-11 11:27:24 +0000

    task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,31 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) { /*throws InterruptedException*/   // Alternative
 
-        CompletableFuture<Void> example = CompletableFuture.runAsync(() -> {
-            System.out.println("Asynchronous!");
+        CompletableFuture<Void> exampleOne = CompletableFuture.runAsync(() -> {
+            System.out.println("Asynchronous One!");
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
 //                throw new RuntimeException(e);
                 e.printStackTrace();
             }
-            System.out.println("Hello world!");
+            System.out.println("Hello first world!");
         });
+
+        CompletableFuture<Void> exampleTwo = CompletableFuture.runAsync(() -> {
+            System.out.println("Asynchronous Two!");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello second world!");
+        });
+
 //        // Ensure the main thread waits for the CompletableFuture to complete         //  Alternative
 //        example.join(); // This blocks the main thread until the async task is done   //  Alternative
         try {
-            Thread.sleep(5000);
+            Thread.sleep(9000);
 //            TimeUnit.SECONDS.sleep(5);        //  Alternative
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
